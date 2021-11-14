5679ef1d5bf62d696f0c20a20184e759e8f3b152
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-15 07:49:02 +0800

    Delete config directory

--- config/lambda-config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//TODO! This is getting used for node-aws-lambda and the Config settings. Probably need to separate/rename.
-module.exports = {
-    //profile: '', // optional for loading AWS credentials from custom profile
-    applicationId: 'amzn1.ask.skill.84d53db3-0605-4c0d-9af7-151fc6e3035f',
-    region: 'us-east-1',
-    functionName: 'RainforestService'
-};
