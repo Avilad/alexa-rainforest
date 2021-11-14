9254b5efb8bae53d9bb59d011df7646c0a1e38bd
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-15 07:50:27 +0800

    Delete src directory

--- src/util.js
+++
@@ -1,6 +0,0 @@
-exports.roundToString = function(number, decimals) {
-    return (Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals)).toFixed(decimals < 0 ? 0 : decimals);
-}
-exports.parseAPINumericalValue = function(object, significandPropertyName) {
-    return parseInt(object[significandPropertyName]) * parseInt(object.Multiplier) / parseInt(object.Divisor);
-}
